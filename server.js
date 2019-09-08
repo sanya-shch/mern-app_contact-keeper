01f33a3b93e8c3acb26877bdcec1321c764b8a9b
sanya-shch <shchipkov.sanya@ukr.net>
2019-09-08 19:29:44 +0300

    0.3 - finished backend

--- server.js
+++ server.js
@@ -14,6 +14,14 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static('client/build'));
+//
+//     app.get('*', (req, res) =>
+//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+//     );
+// }
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server start on port ${PORT}`));
